9b4a46721fc96199181f60637ea3b598ddc70d5b
visibyltech <hassan@zealmart.com>
2026-08-20 20:45:39 +0100

    Update cPanel and Next config to use pre-built folder

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     cpus: 1, // Limit workers to save RAM
   },
